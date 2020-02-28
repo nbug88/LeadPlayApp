850f1475d1ca7261f26264768bd0737a8ec0d0cc
Akhil Naru <anaru@cs.uchicago.edu>
2020-02-28 12:00:37 -0500

    Initial Commit

---
+++ Which cohort are you?/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Which cohort are you?
+//
+//  Created by Akhil Naru on 2/28/20.
+//  Copyright © 2020 Akhil Naru. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
